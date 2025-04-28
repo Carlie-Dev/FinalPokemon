cac1cfbc9bbba493ba53e65e373f2bf64e6e6662
PETERS <cp0973791@dt050565.otc.edu>
2025-04-28 09:25:27 -0500

    Initial Commit

---
+++ FinalPokemon/FinalPokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalPokemonApp.swift
+//  FinalPokemon
+//
+//  Created by PETERS, CARLIE E. on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FinalPokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
